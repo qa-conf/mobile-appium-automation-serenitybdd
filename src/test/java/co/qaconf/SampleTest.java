447e7f9dc9f7d9b7bd1c6d74e4e4b682a461640d
ingjuanfg <juan.isfg@gmail.com>
2025-04-10 19:16:23 -0500

    Linea base iOS

--- src/test/java/co/qaconf/SampleTest.java
+++ src/test/java/co/qaconf/SampleTest.java
@@ -4,6 +4,8 @@ import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.options.XCUITestOptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebElement;
@@ -20,24 +22,22 @@ public class SampleTest {
 
     @BeforeEach
     public void configuracionAppium() throws MalformedURLException {
-        UiAutomator2Options options = new UiAutomator2Options()
-                .setPlatformName("Android")
-                .setDeviceName("R5CR702DMDR")
-                .setPlatformVersion("14")
-                .setApp("https://github.com/saucelabs/my-demo-app-android/releases/download/2.2.0/mda-2.2.0-25.apk")
-                .setIsHeadless(true)
-                .setAutoGrantPermissions(true);
-        options.setAppWaitActivity("com.saucelabs.mydemoapp.android.view.activities.MainActivity");
+        XCUITestOptions options = new XCUITestOptions()
+                .setPlatformName("iOS")
+                .setUdid("00008101-0015254A36A0001E")
+                .setPlatformVersion("18.3.2")
+                .setBundleId("com.google.ios.youtube")
+                .setAutoAcceptAlerts(true);
 
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), options);
+        driver = new IOSDriver(new URL("http://127.0.0.1:4723/"), options);
     }
 
     @Test
     void sampleTestSerenity(){
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement titleProduct = wait.until(
-                ExpectedConditions.presenceOfElementLocated(AppiumBy.id("com.saucelabs.mydemoapp.android:id/productTV"))
+        WebElement lupaYoutube = wait.until(
+                ExpectedConditions.presenceOfElementLocated(AppiumBy.iOSNsPredicateString("name == 'id.ui.navigation.search.button'"))
         );
-        titleProduct.click();
+        lupaYoutube.click();
     }
 }
